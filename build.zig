13f5c29c72c44901815e01261eb30d5a5523e6ef
Jeff DeWall <sr.jilarious@fastmail.net>
2023-12-21 21:37:35 -0700

    Another build.zig.zon change.

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "testz",
+        .name = "testz_main",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
