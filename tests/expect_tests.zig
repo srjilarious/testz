46bc0267702bf5e4fcfc3e7360e4df48a8b28ac2
Jeff DeWall <sr.jilarious@fastmail.net>
2024-06-16 06:37:04 -0600

    Updated misc tests to test their output as well.

--- tests/expect_tests.zig
+++ tests/expect_tests.zig
@@ -4,32 +4,7 @@ const TestFuncInfo = testz.TestFuncInfo;
 const TestContext = testz.TestContext;
 const Printer = testz.Printer;
 
-fn runInternal(func: TestFuncInfo, expectedOutput: []const u8, opts: struct {
-    withColor: bool = false,
-}) !void {
-    _ = opts;
-
-    // Create a test context with a buffer printer so we can check the output.
-    var testContext = TestContext.init(std.heap.page_allocator, .{});
-    defer testContext.deinit();
-
-    // Create a memory printer so we can capture the test output.
-    var printer = try Printer.memory(std.heap.page_allocator);
-    defer printer.deinit();
-
-    _ = try testz.runTests(&.{func}, .{
-        .testContext = &testContext,
-        .writer = printer,
-        .printColor = false, //opts.withColor,
-        // Don't worry about validating stack traces since they are tied to the machine they run on.
-        .printStackTraceOnFail = false,
-        // Replaces the actual time with a dummy ( XX.XX ms) string instead
-        .dummyTiming = true,
-    });
-
-    // Check our test output.
-    try testz.expectEqualStr(expectedOutput, printer.array.array.items);
-}
+const runInternal = @import("./utils.zig").runInternal;
 
 fn expectEqualFailTestInternal() !void {
     try testz.expectEqual(10, 20);
--- tests/misc_tests.zig
+++ tests/misc_tests.zig
@@ -1,25 +1,93 @@
 const std = @import("std");
 const testz = @import("testz");
 
-// pub fn allowNonTestzErrors() !void {
-//     const mem = try std.heap.page_allocator.alloc(u8, 10);
-//     defer std.heap.page_allocator.free(mem);
-//     try testz.expectEqual(true, true);
-// }
-//
-// pub fn alwaysFailTest() !void {
-//     try testz.fail();
-// }
-//
-// pub fn successTest() !void {
-//     try testz.expectEqual(12, 12);
-//     try testz.expectEqualStr("hello", "hello");
-//     try testz.expectNotEqual(10, 20);
-//     try testz.expectNotEqualStr("hello", "world");
-//     try testz.expectTrue(true);
-//     try testz.expectFalse(false);
-// }
-//
-// pub fn skip_notReadyTest() !void {
-//     // nothing to see here.
-// }
+const runInternal = @import("./utils.zig").runInternal;
+
+fn allowNonTestzErrorsInternal() !void {
+    const mem = try std.heap.page_allocator.alloc(u8, 10);
+    defer std.heap.page_allocator.free(mem);
+    try testz.expectEqual(true, true);
+}
+
+pub fn allowNonTestzErrors() !void {
+    const expected: []const u8 =
+        \\
+        \\⋅
+        \\
+        \\1 Passed, 0 Failed, 0 Skipped, 1 Total Tests ( XX.XX ms)
+        \\
+    ;
+
+    try runInternal(.{
+        .func = allowNonTestzErrorsInternal,
+        .name = "allowNonTestzErrors",
+        .group = .{ .name = "Default", .tag = "default" },
+    }, expected, .{});
+}
+
+fn alwaysFailTestInternal() !void {
+    try testz.fail();
+}
+
+pub fn alwaysFailTest() !void {
+    const expected: []const u8 =
+        \\
+        \\X
+        \\FAIL alwaysFailTest: Test hit failure point.
+        \\
+        \\0 Passed, 1 Failed, 0 Skipped, 1 Total Tests ( XX.XX ms)
+        \\
+    ;
+
+    try runInternal(.{
+        .func = alwaysFailTestInternal,
+        .name = "alwaysFailTest",
+        .group = .{ .name = "Default", .tag = "default" },
+    }, expected, .{});
+}
+
+fn successTestInternal() !void {
+    try testz.expectEqual(12, 12);
+    try testz.expectEqualStr("hello", "hello");
+    try testz.expectNotEqual(10, 20);
+    try testz.expectNotEqualStr("hello", "world");
+    try testz.expectTrue(true);
+    try testz.expectFalse(false);
+}
+
+pub fn successTest() !void {
+    const expected: []const u8 =
+        \\
+        \\⋅
+        \\
+        \\1 Passed, 0 Failed, 0 Skipped, 1 Total Tests ( XX.XX ms)
+        \\
+    ;
+
+    try runInternal(.{
+        .func = successTestInternal,
+        .name = "successTest",
+        .group = .{ .name = "Default", .tag = "default" },
+    }, expected, .{});
+}
+
+fn skip_notReadyTestInternal() !void {
+    // nothing to see here.
+}
+
+pub fn testSkipNotReady() !void {
+    const expected: []const u8 =
+        \\
+        \\↷
+        \\
+        \\0 Passed, 0 Failed, 1 Skipped, 1 Total Tests ( XX.XX ms)
+        \\
+    ;
+
+    try runInternal(.{
+        .func = skip_notReadyTestInternal,
+        .name = "skip_notReadyTest",
+        .group = .{ .name = "Default", .tag = "default" },
+        .skip = true,
+    }, expected, .{});
+}
